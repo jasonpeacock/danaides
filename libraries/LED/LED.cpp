004989070d793e48ad22dd9ec7dd21cb71236235
Jason Peacock <jason@jasonpeacock.com>
2015-01-03 18:43:28 -0800

    LED library will be a no-op if initialized with pin 0

--- libraries/LED/LED.cpp
+++ libraries/LED/LED.cpp
@@ -1,12 +1,14 @@
+// system
 #include <Arduino.h>
+#include <inttypes.h>
 
 #include "LED.h"
 
 LED::LED() {
-    _init(-1);
+    _init(0);
 }
 
-LED::LED(int pin) {
+LED::LED(uint8_t pin) {
     _init(pin);
 }
 
@@ -17,39 +19,61 @@ void LED::_resetHeartbeat() {
     _lastHeartbeatTime = millis();
 }
 
-void LED::_init(int pin) {
+void LED::_init(uint8_t pin) {
     _pin = pin;
-    _enabled = false;
+    _on = false;
     _resetHeartbeat();
 }
 
 void LED::setup() {
+    if (!enabled()) {
+        return;
+    }
+
     pinMode(_pin, OUTPUT);
 
     // start with the LED off
     digitalWrite(_pin, LOW);
 }
 
+bool LED::enabled() {
+    return _pin;
+}
+
 // idempotent, don't check current state
 void LED::on() {
-    _enabled = true;
+    _on = true;
+
+    if (!enabled()) {
+        return;
+    }
+
     digitalWrite(_pin, HIGH);
     _resetHeartbeat();
 }
 
 // idempotent, don't check current state
 void LED::off() {
-    _enabled = false;
+    _on = false;
+
+    if (!enabled()) {
+        return;
+    }
+
     digitalWrite(_pin, LOW);
     _resetHeartbeat();
 }
 
 // flash/blink the LED, based on current button LED state
-void LED::flash(int times, int wait) {
-    for (int i = 0; i < times; i++) {
-        _enabled ? digitalWrite(_pin, LOW) : digitalWrite(_pin, HIGH);
+void LED::flash(uint8_t times, uint16_t wait) {
+    if (!enabled()) {
+        return;
+    }
+
+    for (uint8_t i = 0; i < times; i++) {
+        _on ? digitalWrite(_pin, LOW) : digitalWrite(_pin, HIGH);
         delay(wait);
-        _enabled ? digitalWrite(_pin, HIGH) : digitalWrite(_pin, LOW);
+        _on ? digitalWrite(_pin, HIGH) : digitalWrite(_pin, LOW);
 
         if (i + 1 < times) {
             delay(wait);
--- libraries/LED/LED.h
+++ libraries/LED/LED.h
@@ -1,6 +1,9 @@
 #ifndef LED_h
 #define LED_h
 
+// system
+#include <inttypes.h>
+
 /*
  * Constants
  */
@@ -9,26 +12,27 @@
 
 class LED {
     private:
-        int _pin;
+        uint8_t _pin;
         uint32_t _lastHeartbeatTime;
-        bool _enabled;
+        bool _on;
 
-        void _init(int pin);
+        void _init(uint8_t pin);
         void _resetHeartbeat();
 
     public:
         LED();
-        LED(int pin);
+        LED(uint8_t pin);
         ~LED();
 
         // to be called during setup() in main Arduino sketch,
         // will setup the pins properly and anything else.
         void setup();
 
+        bool enabled();
         void on();
         void off();
 
-        void flash(int times, int wait);
+        void flash(uint8_t times, uint16_t wait);
         void thinking();
         void error();
         void heartbeat();
